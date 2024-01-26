06ecc043bbab78489583a88aabcb4f669f2da83b
siddharthpanda99 <siddharthpanda99@gmail.com>
2024-01-26 12:08:57 +0530

    Added a fake login fuunctionality to /login route

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "types/User";
+import { users } from "fake/Users";
 
 const signUpUser = (req: Request, res: Response) => {
     const body: User = req.body;
@@ -10,7 +11,18 @@ const signUpUser = (req: Request, res: Response) => {
 }
 
 const loginUser = (req: Request, res: Response) => {
-    res.status(200).send({message: 'User logged in successfully'})
+    const body: User = req.body;
+    if (!body.email || !body.password) {
+        res.status(403).send({ data: {}, message: 'Please enter username and/or password' })
+    } else {
+
+        const req_user = users.find(user => user.email === body.email && user.password === body.password)
+        if(req_user){
+            res.status(200).send({ message: 'User logged in successfully' })
+        } else {
+            res.status(200).send({ message: 'Check email/password' })
+        }
+    }
 }
 
 export { signUpUser, loginUser };
