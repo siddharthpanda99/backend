734e98008d53f5cde8f04b7663574aa1299cf8cd
siddharthpanda99 <siddharthpanda99@gmail.com>
2024-01-27 13:02:16 +0530

    Fixed an uncommited cors issue

--- src/Index.ts
+++ src/Index.ts
@@ -6,6 +6,7 @@ import pool from "./config";
 import { authRouter, hotelsRouter, userRouter } from "./routes";
 import { warn } from "console";
 import { Logger } from "middlewares/Logger";
+import cors from 'cors';
 // import {appLogger} from "middlewares/Logger";
 // import { notFoundMiddleware, errorHandlerMiddleWare } from "./middleware";
 
@@ -13,6 +14,18 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// Add a list of allowed origins.
+// If you have more origins you would like to add, you can add them to the array below.
+const allowedOrigins = ['http://localhost:5173'];
+
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+
+// Then pass these options to cors:
+app.use(cors(options));
+
 app.use(express.json());
 // app.use(Logger);
 
