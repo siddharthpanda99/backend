80f3566dfb12c41bf0c47d3a2cf4216a28c3c445
siddharthpanda99 <siddharthpanda99@gmail.com>
2024-01-28 10:12:01 +0530

    Added validation logic, validation middleware to login route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express'
 const authRouter = Router();
+import { loginUserValidation, validateLoginUser } from 'validators/authValidators';
 
 import { loginUser, signUpUser } from "controllers/auth";
 
 authRouter.route('/signup').post(signUpUser);
-authRouter.route('/login').post(loginUser);
+
+authRouter.route('/login').post(loginUserValidation, validateLoginUser, loginUser);
 
 export default authRouter;
